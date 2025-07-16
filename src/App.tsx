c0572e681d1a6a77cd7a2910946a47c78200eb60
lucamartinsb <lucabindermartins@gmail.com>
2025-07-16 03:30:36 -0300

    utilizando className para estilizar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-
+import './App.css'
 import AddTask from './components/AddTasks'
 import Tasks from './components/Tasks'
 
 function App() {
   return (
       <div>
-        <h1>Gerenciador de Tarefas</h1>
+        <h1 className='title'>Gerenciador de Tarefas</h1>
         <Tasks/>
         <AddTask/>
       </div>
