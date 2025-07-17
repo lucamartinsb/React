c20fe4ef427f694a2d0dcbfd3032ecaf021b6845
lucamartinsb <lucabindermartins@gmail.com>
2025-07-16 21:15:31 -0300

    estilizando o plano de fundo e o texto principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import AddTask from './components/AddTasks'
-import Tasks from './components/Tasks'
-
 function App() {
   return (
-      <div>
-        <h1 className="text-red-500">Gerenciador de Tarefas</h1>
-        <Tasks/>
-        <AddTask/>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
       </div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
