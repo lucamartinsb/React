cf470fa2657aded02d9ce3f4eb4772a42d599066
lucamartinsb <lucabindermartins@gmail.com>
2025-07-16 22:44:44 -0300

    adicionando hook tasks no componente app e props no compnente tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTasks";
+
 function App() {
+  /*Adicionando Hook. Serve para guardar estados/efeitos da aplicação para usar ou reutilizar em componentes.
+  Elimita a necessidade de utilizar classes, facilita a organização e manutençao de forma mais simples.*/
+  const [tasks, setTask] = useState([
+    {
+      id: 1,
+      title: "Estudar Programação",
+      description: "Estudar programaçãop para se tornar um dev full-stack",
+      isCompleted: false
+    },
+    {
+      id: 2,
+      title: "Estudar Inglês",
+      description: "Estudar ingles para trabalhar pro exterior",
+      isCompleted: false
+    },
+    {
+      id: 3,
+      title: "Fazer academia",
+      description: "Fazer academia pra ficar forte e saudável",
+      isCompleted: false
+    }
+  ]);
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
+      <Tasks tasks={tasks}/> {/* A prop (tasks) será o state {tasks}.
+      Tudo que eu passar dentro da tag do componente, eu terei acesso lá no componente.tsx*/}
       </div>
-    </div>
+    </div>  
   );
 }
 
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,7 +1,10 @@
-function Tasks() {
-return (
-    <h1>Tarefas</h1>
-)
+function Tasks(props) {
+  /*
+    Props (properties) são um mecanismo fundamental para passar dados de um componente pai para um componente filho.
+    São como parametros de uma função, mas para componentes React.
+    Props servem para: 
+    - Passar dados de um pai para um filho. - Reusabilidade de componentes. - Comunicação unidirecional. - Personalização de componentes. - Renderização condicional. */
+  return <h1>{props.tasks[0].title}</h1>;
 }
 
-export default Tasks
+export default Tasks;
