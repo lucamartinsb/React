29e255bf750f358046aa503da85e2cad8108598d
lucamartinsb <lucabindermartins@gmail.com>
2025-08-15 18:22:22 -0300

    Excluído o arquivo Anotations e melhorado os comentários no código

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -11,20 +11,28 @@ function Tasks(props) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {props.tasks.map((task) => (
-        /* a key é essencial para que o React possa identificar de forma única cada item da lista.
-        Isso permite que o React otimize a renderização, saiba quais itens foram adicionados, removidos ou reordenados, e mantenha o estado correto de cada componente da lista.
-        Neste caso, a key que usaremos será o id de cada objeto do state.
-        */
-       // O evento onClick irá invocar a função de atualizar o isCompleted, atualizar o state no App pai e lançar uma linha no texto do botão do item da lista.
+        /*  A key é essencial para que o React possa identificar de forma única cada item da lista.
+            Isso permite que o React otimize a renderização, saiba quais itens foram adicionados, removidos ou reordenados, e mantenha o estado correto de cada componente da lista.
+            Neste caso, a key que usaremos será o id de cada objeto do state. */
         <li key={task.id} className="flex gap-2">
-          <button onClick={() => props.onTaskClick(task.id)} 
-          className={`bg-slate-400 text-white p-2 rounded-md w-full text-left ${task.isCompleted && "line-through"}`}>
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            /* 
+            Para atualizar o estado de um componente pai a partir de um componente filho, precisa-se criar e passar uma função de callback como uma prop para o componente filho.
+            E essa prop sendo uma função que precisa de um argumento (task.id) e de invocá-la em um evento JS, como o onClick(), é necessário usar uma arrow function.
+            Através do template string podemos usar javascript e adicionar tailwind também dentro do className.
+            O evento onClick irá invocar a função onTaskClick() através da props e essa função irá atualizar o estado de Tasks, inverter o valor de isCompleted caso id seja igual ao
+            id da tarefa clicada e se task.isCompleted for true, adiciona o estilo line-through no texto do botão correspondente. */
+            className={`bg-slate-400 text-white p-2 rounded-md w-full text-left ${
+              task.isCompleted && "line-through"
+            }`}
+          >
             {task.title}
-            </button>
-          <button className="bg-slate-400 p-2 rounded-md text-white">
-            <ChevronRightIcon/> 
           </button>
-          </li>
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            <ChevronRightIcon />
+          </button>
+        </li>
       ))}
     </ul>
   );
